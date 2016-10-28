116a0f2e3658119b1838c9e70f254852b4423660
Lennardt Grommel <Lennardt@Grommel.de>
2016-10-28 23:22:38 +0200

    TestTreeviewer hinzugefuegt. Abhaengig von WRBroot/input

---
+++ src/main/java/de/lab4inf/wrb/TestTreeViewer.java
@@ -0,0 +1,27 @@
+package de.lab4inf.wrb;
+
+import java.util.*;
+
+import org.antlr.v4.runtime.*;
+import org.antlr.v4.runtime.tree.*;
+import org.antlr.v4.runtime.tree.gui.*;
+
+public class TestTreeViewer {
+    public static void main(String[] args) {
+        try {
+            showTree("./input");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    public static void showTree(String path) throws Exception{
+        ANTLRInputStream input = new ANTLRFileStream(path);
+        WRBLexer lexer = new WRBLexer(input);
+        CommonTokenStream tokens = new CommonTokenStream(lexer);
+        WRBParser parser = new WRBParser(tokens);
+        ParseTree tree = parser.programm();
+        List<String> ruleNames = Arrays.asList(WRBParser.ruleNames);
+        TreeViewer view = new TreeViewer(ruleNames, tree);
+        view.open();
+    };
+}
