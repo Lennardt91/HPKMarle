d9bd6bec0effbf9347e96c3a76d8f69f2f30da98
Marten <marten@Marten-Laptop.fritz.box>
2016-11-08 17:55:10 +0100

    Java Dock bearbeitet

--- src/main/java/de/lab4inf/wrb/TestTreeViewer.java
+++ src/main/java/de/lab4inf/wrb/TestTreeViewer.java
@@ -5,7 +5,11 @@ import java.util.*;
 import org.antlr.v4.runtime.*;
 import org.antlr.v4.runtime.tree.*;
 import org.antlr.v4.runtime.tree.gui.*;
-
+/**
+ * Klasse zum anzeigen eines Syntax Baums
+ * @author Lenard 
+ *
+ */
 public class TestTreeViewer {
     public static void main(String[] args) {
         try {
