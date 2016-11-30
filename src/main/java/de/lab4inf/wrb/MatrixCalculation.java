36bb263b667b648ca98def24b5fff4ad5da82833
Lennardt Grommel <Lennardt@Grommel.de>
2016-11-30 15:47:15 +0100

    Geruest fuer parallele Berechnung(Var 1) der Matrizen hinzuegefuegt.

--- src/main/java/de/lab4inf/wrb/MatrixCalculation.java
+++ src/main/java/de/lab4inf/wrb/MatrixCalculation.java
@@ -1,14 +1,21 @@
 package de.lab4inf.wrb;
 /**
- * Klasse für berechnungen mit WRBMatrix Matrizen
- * @author marten lenard
+ * Klasse fuer Berechnungen mit WRBMatrix Matrizen
+ * @author marten lennardt
  *
  */
 public final class MatrixCalculation {
 
+	/**
+	 * Multipliziert 2 Matrizen parallel
+	 * @param A Erste WRBMatrix
+	 * @param B Zweite WRBMatrix
+	 * @return Ergebnis als WRBMatrix
+	 * @throws IllegalArgumentException
+	 */
 	public static WRBMatrix matSeriell(WRBMatrix A, WRBMatrix B) throws IllegalArgumentException {
 		if(A.getColumnCount()!=B.getRowCount())
-			throw new IllegalArgumentException("Wrong Matrix size");
+			throw new IllegalArgumentException("Wrong matrix size. AColSize:" + A.getColumnCount() + " BRowSize:" + B.getRowCount());
 		double [][]a = A.getMatrix();
 		double [][]b = B.getMatrix();
 		double [][]c = new double [A.getRowCount()][B.getColumnCount()];
@@ -26,4 +33,27 @@ public final class MatrixCalculation {
 		return new WRBMatrix(c);
 	}
 
+	/**
+	 * Multipliziert 2 Matrizen parallel(Algorithmus 1 von Praktikumsaufgabe)
+	 * @param A Erste WRBMatrix
+	 * @param B Zweise WRBMatrix
+	 * @return Ergebnis als WRBMatrix
+	 * @throws IllegalArgumentException
+	 */	
+	public static WRBMatrix matParallel1(WRBMatrix A, WRBMatrix B) throws IllegalArgumentException{
+		if(A.getColumnCount() != B.getRowCount())
+			throw new IllegalArgumentException("Wrong matrix size. AColSize:" + A.getColumnCount() + " BRowSize:" + B.getRowCount());
+		
+		double [][]a = A.getMatrix();
+		double [][]b = B.getMatrix();
+		double [][]c = new double [A.getRowCount()][B.getColumnCount()];
+		
+		//TODO: Noch keine parallele Ausfuehrung
+		for (int i = 1; i<A.getRowCount(); i++)
+			for (int j = 1; j<B.getColumnCount(); j++)
+				for (int k = 1; k<A.getColumnCount(); k++)
+					c[i][j] += a[i][k]*b[k][j];
+		
+		return new WRBMatrix(c);
+	}
 }
