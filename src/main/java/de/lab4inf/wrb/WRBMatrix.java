d793c33b474caf69d6a723dc84ea723ca5ce09ec
Lennardt Grommel <Lennardt@Grommel.de>
2016-11-30 20:07:29 +0100

    Rnd Matrizen koennen nun mit dem jeweiligen Konstruktor erzeugt werden.
    Matrix(n rows, n cols) und Matrix() fuer ebenfalls zufaellige Groeße der
    Matrix

--- src/main/java/de/lab4inf/wrb/WRBMatrix.java
+++ src/main/java/de/lab4inf/wrb/WRBMatrix.java
@@ -11,7 +11,9 @@ public class WRBMatrix {
 	int m;  //Anzahl Zeilen der Matrix
 	int n; //Anzahl Spalten der Matrix
 	double[][] matrix; //Matrix als 2 dimensionales Array
-
+	private int maxFieldValue = 1000; // Maximaler und minimaler Wert, den ein Feld der Matrix annehmen kann
+	private int maxRowOrColSizeValue = 5; //Maximaler Wert den eine Reihe oder Spalte annehmen kann
+	
 	/**
 	 * Konstruktor
 	 * 
@@ -22,6 +24,47 @@ public class WRBMatrix {
 		// Zeilen m und Spalten n bestimmen
 		this.m = matrix.length;
 		this.n = matrix[0].length ;
+	}
+	
+	/**
+	 * Konstruktor fuer random Matrix in gewuenschter Groesse
+	 * @param rows Anzahl der Reihen
+	 * @param cols Anzahl der Spalten
+	 */
+	public WRBMatrix(int rows, int cols){
+		this.m = rows;
+		this.n = cols;
+		
+		this.matrix = new double[m][n];
+		for (int i = 0; i<m; i++)
+			for (int j = 0; j<n; j++)
+				matrix[i][j] = getRndFieldValue();
+	}
+	
+	/**
+	 * Konstruktor fuer random Matrix in random Groesse(beschraenkt auf maximal 5 Spalten und Reihen)
+	 */
+	public WRBMatrix(){
+		this.m = this.getRndRowOrColSize();
+		this.n = this.getRndRowOrColSize();
+		
+		
+		this.matrix = new double[m][n];
+		for (int i = 0; i<m; i++)
+			for (int j = 0; j<n; j++)
+				matrix[i][j] = getRndFieldValue();
+	}
+	
+	/**
+	 * Gibt einen Zufaelligen Wert fuer ein Feld zurueck
+	 * @return zufaelliger Wert im Rahmen des WRBMatrix rndFieldValue
+	 */
+	private int getRndFieldValue(){
+		return (int)Math.random()*maxFieldValue*2 - maxFieldValue; //RND Zahl von -rndFieldValue bis +rndFieldValue
+	}
+	
+	private int getRndRowOrColSize(){
+		return (int)Math.random()*maxRowOrColSizeValue + 1;
 	}
 
 	/**
