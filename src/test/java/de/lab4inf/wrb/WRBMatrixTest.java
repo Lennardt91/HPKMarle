bdeb788ea2c9da2594988c9115a9962dbe61f3fc
Marten <marten@Marten-Laptop.fritz.box>
2016-11-30 19:48:49 +0100

    Timing Test und andere Test hinzugefügt

--- src/test/java/de/lab4inf/wrb/WRBMatrixTest.java
+++ src/test/java/de/lab4inf/wrb/WRBMatrixTest.java
@@ -2,10 +2,12 @@ package de.lab4inf.wrb;
 
 import static org.junit.Assert.*;
 
+import java.util.HashMap;
+
 import org.junit.Test;
 
 public class WRBMatrixTest {
-	public WRBMatrix generateMatrix(int m, int n) {
+	public WRBMatrix GenerateMatrix(int m, int n) {
 		double[][] a = new double[m][n];
 		for (int i = 0; i < m; i++) {
 			for (int j = 0; j < n; j++) {
@@ -50,6 +52,54 @@ public class WRBMatrixTest {
 		WRBMatrix A = new WRBMatrix(a), B = new WRBMatrix(b), C = new WRBMatrix(c), result;
 		result=A.matSeriell(B);
 		assertEquals(true, result.equals(C));
+	}
+	@Test(expected=IllegalArgumentException.class)
+	public void TestWrongDimension(){
+		WRBMatrix A = GenerateMatrix(4, 5),B = GenerateMatrix(4, 5),C = null;
+		C = A.matSeriell(B);
+	}
+	@Test(expected=IllegalArgumentException.class)
+	public void TestWrongDimension2(){
+		WRBMatrix A = GenerateMatrix(1,1),B = GenerateMatrix(10,10),C =null;
+		C = A.matSeriell(B);
+	}
+	@Test
+	public void TimingTest(){
+		HashMap<Integer, WRBMatrix> A = new HashMap<>();
+		HashMap<Integer, WRBMatrix> B = new HashMap<>();
+		HashMap<Integer, WRBMatrix> C = new HashMap<>();
+		HashMap<Integer, Long> serieltimes = new HashMap<>();
+		HashMap<Integer, Long> paraleltimes = new HashMap<>();
+		WRBMatrix a,b,c;
+		int maxsize = 2048;
+		int scale = -(int) Math.pow(10, 6);
+		long time = 0;
+		for(int i = 64;i<2049;i*=2){
+			A.put(i, GenerateMatrix(i, i));
+			B.put(i, GenerateMatrix(i, i));
+		}
+		
+		for(int i = 64;i<=maxsize;i*=2){
+			a = A.get(i);
+			b = B.get(i);
+			time = System.nanoTime();
+			c = a.matSeriell(b);
+			time-= System.nanoTime();
+			C.put(i, c);
+			serieltimes.put(i, (time/scale));
+		}
+		Ausgabe(serieltimes,paraleltimes,"seriel");
+		
+		
+	}
+	public void Ausgabe (HashMap<Integer, Long>serieltime,HashMap<Integer, Long> paraltime,String name){
+		String format = name+"\nn\t\t|t seq\t\t|t pall\t\t|s(n)";
+		int sice = (int) Math.pow(2,5+serieltime.size());
+		for(int i = 64;i<=sice;i*=2){
+			format+="\n"+i+"\t\t|"+serieltime.get(i)+"ms\t\t|"+0+"ms\t\t|"+0+"ms";
+		}
+		System.out.println(format);
+		
 	}
 
 }
